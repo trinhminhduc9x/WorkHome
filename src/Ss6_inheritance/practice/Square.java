0c3f510927e005f536f51b2fa18e802915f400ef
Your Name <duc123456789th@gmail.com>
2022-06-13 23:40:47 +0700

    add

---
+++ src/Ss6_inheritance/practice/Square.java
@@ -0,0 +1,33 @@
+package Ss6_inheritance.practice;
+
+public class Square extends Rectangle {
+    public Square(){
+
+    }
+    public Square(double side) {
+        super(side, side);
+    }
+    public Square(String color, boolean filled, double side) {
+        super(color, filled, side, side);
+    }
+    public double getSide(){
+        return getWidth();
+    }
+    public void setSide(double side){
+        setWidth(side);
+        setLength(side);
+    }
+
+    @Override
+    public void setWidth(double width) {
+       setSide(width);
+    }
+
+    @Override
+    public String toString() {
+        return "A Square with side="
+                + getSide()
+                + ", which is a subclass of "
+                + super.toString();
+    }
+}
---
+++ src/Ss6_inheritance/practice/SquareTest.java
@@ -0,0 +1,16 @@
+package Ss6_inheritance.practice;
+
+
+
+class SquareTest {
+    public static void main(String[] args) {
+        Square square = new Square();
+        System.out.println(square);
+
+        square = new Square(2.3);
+        System.out.println(square);
+
+        square = new Square("xanh", true, 5.0);
+        System.out.println(square);
+    }
+}
